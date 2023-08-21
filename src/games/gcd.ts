581fd4ca8c17535c07065a59c1339b855688ca5a
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-08-21 20:43:22 +0300

    feat: implement brain-gcd game

---
+++ src/games/gcd.ts
@@ -0,0 +1,31 @@
+import gamebase from '../index.js';
+import getRandomInteger from '../randomNumGen.js';
+import descriptions from '../descriptions.js';
+import { MAX_NUMBER_FOR_RANDOM_GEN } from '../constants.js';
+import type { Task } from '../typings.js';
+
+const calculateGcd = (num1: number, num2: number): number => {
+    const remainder = num1 % num2;
+
+    if (remainder === 0) {
+        return num2;
+    }
+
+    return calculateGcd(num2, remainder);
+  };
+
+function getTask(): Task {
+    const number1 = getRandomInteger(MAX_NUMBER_FOR_RANDOM_GEN);
+    const number2 = getRandomInteger(MAX_NUMBER_FOR_RANDOM_GEN);
+
+    const question = `${number1} ${number2}`;
+    const expectedResult = calculateGcd(number1, number2).toString();
+
+    return { question, expectedResult };
+}
+
+function gcdGame(): void {
+    gamebase(descriptions.gcd, getTask);
+}
+
+export default gcdGame;
