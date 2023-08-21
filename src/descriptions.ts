58d31b619d6aa94da628520876e6eadc44fe3dce
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-08-21 20:28:13 +0300

    feat: add description for brain-gcd game

--- src/descriptions.ts
+++ src/descriptions.ts
@@ -1,4 +1,5 @@
 export default {
     even: 'Answer "yes" if the number is even, otherwise answer "no".',
     calc: 'What is the result of the expression?',
+    gcd: 'Find the greatest common divisor of given numbers.',
 };
