32a06f9dfc6b9a5a70f29fa03cd8f9195b6e5f60
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2023-08-21 19:05:08 +0300

    feat: create alias types for brain-calc game

--- src/typings.ts
+++ src/typings.ts
@@ -8,3 +8,10 @@ export type Task = {
 };
 
 export type GetTask = () => Task;
+
+/* For brain-calc */
+export type Sign = '+' | '-' | '*' | '%';
+export type SignMapFn = (a: number, b: number) => number;
+export type SignMap = Record<Sign, SignMapFn>;
+
+export type CalculateFn = (num1: number, num2: number, sign: keyof SignMap) => number;
